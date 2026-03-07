19fe3fa7af31529dbb05124d8e10d87766a0b016
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-03-07 13:10:34 -0300

    Update app_emergencia.py

--- app_emergencia.py
+++ app_emergencia.py
@@ -161,7 +161,7 @@ def get_data(file_input):
                 df = pd.read_excel(file_input, parse_dates=["Fecha"])
         else:
             # 🌐 Conexión directa a la carpeta de GitHub por defecto
-            github_url = "https://raw.githubusercontent.com/PREDWEEM/loliumTA_2026/main/meteo_daily.csv"
+            github_url = "https://raw.githubusercontent.com/PREDWEEM/LOLIUM_LARTIGAU-2026/main/meteo_daily.csv"
             try:
                 df = pd.read_csv(github_url, parse_dates=["Fecha"])
             except Exception:
